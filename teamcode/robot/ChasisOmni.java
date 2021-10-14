4ac28579a593b5ea127e6bf43a1f0b8157180020
Ernesto Miranda <55798237+ErnoMitrovic@users.noreply.github.com>
2021-10-13 21:58:22 -0500

    Ajusté valores del movimiento
    Ver imagen en discord

--- teamcode/robot/ChasisOmni.java
+++ teamcode/robot/ChasisOmni.java
@@ -23,9 +23,9 @@ public class ChasisOmni implements Mechanism{
     public void move(double drive, double lateral, double turn){
         // TODO: Add variables for the other motors using the correct arithmetic
         double frontLeftPower = drive + lateral + turn;
-        double frontRightPower = drive + lateral + turn;
-        double backLeftPower = drive + lateral + turn;
-        double backRightPower = drive + lateral + turn;
+        double frontRightPower = drive - lateral - turn;
+        double backLeftPower = drive - lateral + turn;
+        double backRightPower = drive + lateral - turn;
         // TODO: Assign the corresponding power to each motor
         frontLeft.setPower(frontLeftPower);
         frontRight.setPower(frontRightPower);
