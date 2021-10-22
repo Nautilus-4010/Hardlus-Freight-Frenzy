012329a85d3d303a9616e08f44f3cffbf64264ec
Arturo Mendoza Gutiérrez <71675966+Arturo-M12@users.noreply.github.com>
2021-10-22 12:11:28 -0500

    Correcciones ToDo y espacio
    22/10/2021: Correcciones ToDo y espacio

--- teamcode/robot/ChasisOmni.java
+++ teamcode/robot/ChasisOmni.java
@@ -21,11 +21,10 @@ public class ChasisOmni implements Mechanism{
     }
     
     public void move(double drive, double lateral, double turn, double multiplier){
-        // TODO: Include multiplier somewhere to increase or decrease the speed of the robot
-        double frontLeftPower = (drive + lateral + turn)*multiplier;
-        double frontRightPower = (drive - lateral - turn)*multiplier;
-        double backLeftPower = (drive - lateral + turn)*multiplier;
-        double backRightPower = (drive + lateral - turn)*multiplier;
+        double frontLeftPower = (drive + lateral + turn) * multiplier;
+        double frontRightPower = (drive - lateral - turn) * multiplier;
+        double backLeftPower = (drive - lateral + turn) * multiplier;
+        double backRightPower = (drive + lateral - turn) * multiplier;
         frontLeft.setPower(Range.clip(frontLeftPower, -1, 1));
         frontRight.setPower(Range.clip(frontRightPower, -1, 1));
         backLeft.setPower(Range.clip(backLeftPower, -1, 1));
