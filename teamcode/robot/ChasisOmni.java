12074c637244093a4ce88d7d32e1d9cf2a12d6f7
Arturo Mendoza Gutiérrez <71675966+Arturo-M12@users.noreply.github.com>
2021-10-21 21:52:43 -0500

    Correcciones 21/10/2021
    Se agregaron los nombres correctos de los motores conforme a aquellos del robot controller. Además, se añadieron los multiplicadores para que el chasis reduzca su velocidad dependiendo de que bumpers se presionan.

--- teamcode/robot/ChasisOmni.java
+++ teamcode/robot/ChasisOmni.java
@@ -15,20 +15,18 @@ public class ChasisOmni implements Mechanism{
     public ChasisOmni(){}
     
     public void initializeHardware(HardwareMap hardwareMap){
-        frontLeft = hardwareMap.get(DcMotor.class, "Motor_frontal_izquierdo");
-        frontRight = hardwareMap.get(DcMotor.class, "Motor_frontal_derecho");
-        backLeft = hardwareMap.get(DcMotor.class, "Motor_trasero_izquierdo");
-        backRight = hardwareMap.get(DcMotor.class, "Motor_trasero_derecho");
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft = hardwareMap.get(DcMotor.class, "Motor frontal izquierdo");
+        frontRight = hardwareMap.get(DcMotor.class, "Motor frontal derecho");
+        backLeft = hardwareMap.get(DcMotor.class, "Motor trasero izquierdo");
+        backRight = hardwareMap.get(DcMotor.class, "Motor trasero derecho");
     }
     
     public void move(double drive, double lateral, double turn, double multiplier){
         // TODO: Include multiplier somewhere to increase or decrease the speed of the robot
-        double frontLeftPower = drive + lateral + turn;
-        double frontRightPower = drive - lateral - turn;
-        double backLeftPower = drive - lateral + turn;
-        double backRightPower = drive + lateral - turn;
+        double frontLeftPower = (drive + lateral + turn)*multiplier;
+        double frontRightPower = (drive - lateral - turn)*multiplier;
+        double backLeftPower = (drive - lateral + turn)*multiplier;
+        double backRightPower = (drive + lateral - turn)*multiplier;
         frontLeft.setPower(Range.clip(frontLeftPower, -1, 1));
         frontRight.setPower(Range.clip(frontRightPower, -1, 1));
         backLeft.setPower(Range.clip(backLeftPower, -1, 1));
